16e21b386678c514e540ff42074b3a90b29edbfc
Ina Nivald <inanivald@Inas-MacBook-Air-2.local>
2021-01-07 15:03:15 +0100

    Added feature for edit of album title.

--- photo-app/src/components/albums/Album.js
+++ photo-app/src/components/albums/Album.js
@@ -1,28 +1,49 @@
-import React from 'react'
+import { useState } from 'react';
 import { useParams } from 'react-router-dom'
-import { ClipLoader } from 'react-spinners'
-import ImagesGrid from './ImagesGrid'
+import ImagesView from './ImagesView'
 import useAlbum from '../../hooks/useAlbum'
 import UploadAlbumImage from './UploadAlbumImage'
+import EditAlbumTitle from './EditAlbumTitle'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPen } from '@fortawesome/free-solid-svg-icons'
+import { ClipLoader } from 'react-spinners'
 
 const Album = () => {
 	const { albumId } = useParams()
 	const { album, images, loading } = useAlbum(albumId)
+	const [editTitle, setEditTitle] = useState(false);
+
+	const handleEditTitle = () => {
+        setEditTitle(true);
+	};
 
 	return (
-		<>
-			<h2 className="mb-3">Album {album && album.title}</h2>
-
-			<UploadAlbumImage albumId={albumId} />
-
-			<hr />
-
+		<>	
 			{loading
-				? (<ClipLoader color={"#888"} size={20} />)
-				: (<ImagesGrid images={images} />)
+				? (<ClipLoader/>)
+				: album && 
+					<>
+						{editTitle 
+							? <EditAlbumTitle album={album}/> 
+							: <>
+								<h2> {album.title} 
+								<span 
+									className="edit_album_title" 
+									onClick={handleEditTitle}>
+									<FontAwesomeIcon icon={faPen} size="xs"/>
+								</span>
+								</h2>
+
+								<UploadAlbumImage albumId={albumId} />
+								<ImagesView images={images} />
+								
+							</>
+						}
+					</>
 			}
 		</>
 	)
 }
+
 
 export default Album
