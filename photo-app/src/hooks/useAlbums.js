af6aa67703a2e49630b2f90a1c81f0af311892da
Ina Nivald <inanivald@Inas-MacBook-Air-2.local>
2021-01-07 17:29:30 +0100

    Changed useAlbums so only owners can see their own alby

--- photo-app/src/hooks/useAlbums.js
+++ photo-app/src/hooks/useAlbums.js
@@ -1,28 +1,33 @@
 import { useEffect, useState } from 'react'
 import { db } from '../firebase'
+import { useAuth } from '../contexts/AuthContext'
 
 const useAlbums = () => {
 	const [albums, setAlbums] = useState([])
+	const { currentUser } = useAuth()
 	const [loading, setLoading] = useState(true)
 
 	useEffect(() => {
-		const unsubscribe = db.collection('albums').orderBy('title').onSnapshot(snapshot => {
-			setLoading(true)
-			const snapshotAlbums = []
+		// Snapshot listener for all of the user's albums in firebase
+		const unsubscribe = db.collection('albums')
+			.where('owner', '==', currentUser.uid)
+			.onSnapshot(snapshot => {
+				setLoading(true)
+				const snapshotAlbums = []
 
-			snapshot.forEach(doc => {
-				snapshotAlbums.push({
-					id: doc.id,
-					...doc.data(),
+				snapshot.forEach(doc => {
+					snapshotAlbums.push({
+						id: doc.id,
+						...doc.data(),
+					})
 				})
-			})
 
 			setAlbums(snapshotAlbums)
 			setLoading(false)
 		})
 
 		return unsubscribe
-	}, [])
+	}, [currentUser.uid])
 
 	return { albums, loading }
 }
